072de0f1b8e369d470f79331947b941026b3692e
shirishgund <31992242+shirishgund@users.noreply.github.com>
2024-06-20 19:11:09 +0530

    Update buildTest.groovy

--- vars/buildTest.groovy
+++ vars/buildTest.groovy
@@ -1,8 +1,9 @@
 def call(Map config) {
+  stage("buildscan"){
 echo 'echo hi how are you'
 String jira=(config.jira_id)
 String branch=(config.branch)
 echo  'jira id is' + jira
 echo  'branch  is' + branch
-  
-  }
+  }  
+}
